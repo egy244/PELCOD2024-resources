7150efb3e7ecf162df56f5919c8320c1df1e24c8
virgiawanliswanto <iwanfirgiawan82@gmail.com>
2024-09-21 15:15:23 +0700

    latihan coding tgl 21 september 2024

---
+++ main.py
@@ -0,0 +1,31 @@
+# nilai = int(input("masukkan nilai: "))
+# print(nilai)
+
+# nilai = nilai + 5
+# print(nilai)
+# if (nilai > 80):
+#     print ("selamat anda lulus")
+# elif (nilai > 90 and nilai <= 100):
+#         print ("selamat anda lulus, dengan nilai yang memuaskan")
+# else:
+#     print ("anda masih belum lulus tes, semangat dan coba lagi")
+
+
+#for loop
+# for i in range(10,0,-2):
+#     print(i)
+#     print("hello world")
+
+# angka = 30
+# while(angka >= 1):
+#     print(angka)
+#     angka -= 1
+
+def sayhello():
+    print("hello rain")
+
+sayhello()
+sayhello()
+
+fungsi_2kali2 lambda
+
